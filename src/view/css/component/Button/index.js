36cc4227475d35304183589c7a40ae80885afb11
十一 <1318720440@qq.com>
2023-08-15 21:43:51 +0800

    fix: 添加自动过度

--- src/view/css/component/Button/index.js
+++ src/view/css/component/Button/index.js
@@ -1,5 +1,42 @@
+import { Button } from "antd";
 import "./index.css";
 function Botton() {
-  return <div>按钮风格</div>;
+  setTimeout(() => {
+    initTransiton();
+    console.log("lal");
+  }, 4000);
+  const initTransiton = () => {
+    const btn = document.getElementById("btn");
+    const detail = document.querySelector(".detail");
+    btn.onmouseenter = function () {
+      detail.style.height = "auto";
+      const { height } = detail.getBoundingClientRect();
+      detail.style.height = "0";
+      detail.style.transition = ".4s";
+      // detail.offsetHeight;
+      requestAnimationFrame = function () {
+        console.log("lail");
+        detail.style.height = height + "px";
+      };
+      requestAnimationFrame();
+    };
+    btn.onmouseleave = function () {
+      detail.style.height = "0";
+      detail.style.transition = ".3s";
+    };
+    console.log("lai l");
+  };
+
+  return (
+    <div>
+      <div className="item">
+        <h2>高度自动过度</h2>
+        <Button id="btn">自动过度</Button>
+        <div className="detail">
+          明月几时有 把酒问青天 不知天上宫阙 今夕是何年
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Botton;
