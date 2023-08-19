b58d232538d2815cf4cff299db8d738cf37660aa
十一 <1318720440@qq.com>
2023-08-19 20:21:53 +0800

    fix: 添加卡片确认图表

--- src/view/card/components/cardList/index.js
+++ src/view/card/components/cardList/index.js
@@ -1,7 +1,22 @@
-function CardList() {
+import "./index.css";
+function CardItem() {
   return (
     <>
-      <div>开店</div>
+      <div className="card_item">
+        <div className="card_item_scuess"></div>
+      </div>
+    </>
+  );
+}
+function CardList() {
+  const list = [1, 2, 3, 4, 4, 11, 56, 7, 8, 9, 0, 1];
+  return (
+    <>
+      <div className="card_">
+        {list.map((item, index) => {
+          return <CardItem key={index}></CardItem>;
+        })}
+      </div>
     </>
   );
 }
